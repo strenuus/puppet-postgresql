7f581183a717f22489d2089491c588e6c38d4192
Marshal Simmons <marshal.simmons@gmail.com>
2013-02-19 22:33:52 -0600

    Correct postgres url

--- files/brews/postgresql.rb
+++ files/brews/postgresql.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Postgresql < Formula
   homepage 'http://www.postgresql.org/'
-  url 'http://ftp.postgresql.org/pub/source/v9.2.2/postgresql-9.2..tar.bz2'
+  url 'http://ftp.postgresql.org/pub/source/v9.2.2/postgresql-9.2.2.tar.bz2'
   sha1 'c75fd5696af02a275a104260eac8b3a4abe35682'
   version '9.2.2-boxen2'
 
